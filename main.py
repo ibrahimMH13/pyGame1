dea813a3392497d320153c6cc1cc9a83546b0c50
Ibrahim I Musabah <i.musabah92@gmail.com>
2022-08-12 22:56:30 +0800

    test logic game

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from deck import Deck
-
+from player import Player
 new_deck = Deck()
 first_card = new_deck.all_cards[0]
 last_card = new_deck.all_cards[-1]
@@ -14,9 +14,20 @@ last_card = new_deck.all_cards[-1]
 print(first_card, last_card)
 length = len(new_deck.all_cards)
 print(length)
-while length <= 52:
-    deal_card = new_deck.deal_on()
-    print(f"deal card is# {deal_card}")
-    print(f"total card#  {len(new_deck.all_cards)}")
-    if len(new_deck.all_cards) == 0:
-        break
+deal_card = new_deck.deal_on()
+
+#while length <= 52:
+#    deal_card = new_deck.deal_on()
+#    print(f"deal card is# {deal_card}")
+#    print(f"total card#  {len(new_deck.all_cards)}")
+#    if len(new_deck.all_cards) == 0:
+#        break
+
+ibrahim = Player('Ibrahim')
+ibrahim.add_cards(deal_card)
+print(ibrahim)
+print(ibrahim.drop_card())
+ibrahim.add_cards([deal_card,deal_card,deal_card,deal_card])
+print(ibrahim)
+ibrahim.drop_card()
+print(ibrahim)
